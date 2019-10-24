e3a6375811ad8903be2c483002bc69ebaf051c70
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-10-23 20:52:56 -0500

    Added a box mesh generator

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
 from meshes.curved import trapezoid
+from meshes.box import box01,reference
 from mayavi import mlab
 
 N=20
 
-X,Y,Z=trapezoid(N)
+#X,Y,Z=trapezoid(N)
+#X,Y,Z=box01(N)
+X,Y,Z=reference(N)
 
-#mlab.figure()
-#mlab.points3d(X,Y,Z,0*X+0*Y+0*Z,scale_mode="none",scale_factor=0.01)
-#mlab.axes()
-#mlab.show()
-
+mlab.figure()
+mlab.points3d(X,Y,Z,X+0*Y+0*Z,scale_mode="none",scale_factor=0.01)
+mlab.axes()
+mlab.show()
---
+++ meshes/box.py
@@ -0,0 +1,58 @@
+import numpy as np
+from quadrature import gauss_lobatto
+from interpolation import lagrange
+
+def reference(M):
+    x=np.array([-1.0,1.0,-1.0,1.0,-1.0,1.0,-1.0,1.0])
+    y=np.array([-1.0,-1.0,1.0,1.0,-1.0,-1.0,1.0,1.0])
+    z=np.array([-1.0,-1.0,-1.0,-1.0,1.0,1.0,1.0,1.0])
+    return box(x,y,z,M)
+
+def box01(M):
+    x=np.array([0.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0])
+    y=np.array([0.0,0.0,1.0,1.0,0.0,0.0,1.0,1.0])
+    z=np.array([0.0,0.0,0.0,0.0,1.0,1.0,1.0,1.0])
+    return box(x,y,z,M)
+
+def box(x,y,z,M):
+    N=1
+    n=N+1
+    nn=n*n
+    m=M+1
+    mm=m*m
+
+    zn,wn=gauss_lobatto(N)
+    zm,wm=gauss_lobatto(M)
+    J=lagrange(zm,zn)
+
+    x=x.reshape((nn,n))
+    y=y.reshape((nn,n))
+    z=z.reshape((nn,n))
+    Jx=np.dot(x,J.T)
+    Jy=np.dot(y,J.T)
+    Jz=np.dot(z,J.T)
+
+    x=Jx.reshape((n,n,m))
+    y=Jy.reshape((n,n,m))
+    z=Jz.reshape((n,n,m))
+
+    Jx=np.zeros((n,m,m))
+    Jy=np.zeros((n,m,m))
+    Jz=np.zeros((n,m,m))
+    for i in range(n):
+      Jx[i,:,:]=np.dot(J,x[i,:,:])
+      Jy[i,:,:]=np.dot(J,y[i,:,:])
+      Jz[i,:,:]=np.dot(J,z[i,:,:])
+    x=Jx.reshape((n,mm))
+    y=Jy.reshape((n,mm))
+    z=Jz.reshape((n,mm))
+
+    Jx=np.dot(J,x)
+    Jy=np.dot(J,y)
+    Jz=np.dot(J,z)
+
+    x=Jx.reshape((m,m,m))
+    y=Jy.reshape((m,m,m))
+    z=Jz.reshape((m,m,m))
+
+    return x,y,z
