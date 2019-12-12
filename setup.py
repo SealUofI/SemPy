f78a05fdfafa559dd1a87891729364ed1485322e
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-12-11 22:00:22 -0600

    [packaging] Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup,find_packages
 
 setup(name='sempy',
-      packages=find_packages(),
-      install_requires=[
-          'numpy',
-      ]
+    version='0.1',
+    description='SemPy - Spectral Element Methods in Python',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.17.3',
+        'meshio==3.3.0'
+    ]
 )
