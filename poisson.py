20af1703f63444f562c454a971d91a4b0e03145b
Thilina Rathnayake <thilinarmtb@gmail.com>
2019-12-15 02:03:58 -0600

    Update poisson.py

--- poisson.py
+++ poisson.py
@@ -7,29 +7,43 @@ from sempy.mesh import load_mesh
 from sempy.gradient import gradient,gradient_2d,\
     gradient_transpose,gradient_transpose_2d
 
-from sempy.iterative import cg,pcg
+from sempy.elliptic import elliptic_cg
 
 from mayavi import mlab
 import matplotlib.pyplot as plt
 
-N=3
+plot_on=0
+
+N=10
 n=N+1
 
-mesh=load_mesh("box002.msh")
+mesh=load_mesh("box008.msh")
 mesh.find_physical_coordinates(N)
 mesh.establish_global_numbering()
 mesh.calc_geometric_factors()
 mesh.setup_mask()
 
-X=mesh.xe
-Y=mesh.ye
-Z=mesh.ze
+nelem=mesh.get_num_elems()
+Np=mesh.Np
 
-u=0*X+1
-u=mesh.apply_mask(u)
+X=mesh.get_x()
+Y=mesh.get_y()
+Z=mesh.get_z()
 
-example_2d=0
-plot_on   =1
+J=mesh.get_jaco()
+B=mesh.get_mass()
+
+x=np.sin(np.pi*X)*np.sin(np.pi*Y)*np.sin(np.pi*Z)
+x=mesh.apply_mask(x)
+
+b=3*np.pi*np.pi*np.sin(np.pi*X)*np.sin(np.pi*Y)*np.sin(np.pi*Z)
+b=b*B*J
+b=mesh.dssum(b)
+b=mesh.apply_mask(b)
+
+x_cg,niter=elliptic_cg(mesh,b,tol=1e-8,maxit=1000,verbose=0)
+
+assert np.allclose(x,x_cg,1e-8)
 
 if plot_on:
     if example_2d:
