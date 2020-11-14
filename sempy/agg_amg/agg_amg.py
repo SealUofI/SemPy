02e3b48ae47ed5e99aeb7c2e49b0c288ff159775
Christensen <njchris2@illinois.edu>
2020-11-14 15:32:36 -0600

    add call to stiffness mat

--- sempy/agg_amg/agg_amg.py
+++ sempy/agg_amg/agg_amg.py
@@ -1,3 +1,10 @@
 from sempy.agg_amg.J0 import get_J0
+from sempy.agg_amg.stiffness_mat import stiffness_mat
+import numpy as np
 
+pts = np.loadtxt("pts.dat")
+tri = np.loadtxt("tri.dat",dtype=np.int32) - 1
+   
+
+AL, BL, Q, R, xb, yb, p, t =  stiffness_mat(pts,tri)
 J0 = get_J0(x, y)
