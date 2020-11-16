2bfaaba535f5a6dbd3ea980bb55370dbe00a5a56
Christensen <njchris2@illinois.edu>
2020-11-16 13:13:25 -0600

    use sparse dot

--- sempy/agg_amg/agg_amg.py
+++ sempy/agg_amg/agg_amg.py
@@ -7,13 +7,12 @@ tri = np.loadtxt("tri.dat", dtype=np.int32) - 1
 
 AL, BL, Q, R, xb, yb, p, t = stiffness_mat(pts, tri)
 
-Ab = np.dot(Q.T, np.dot(AL, Q))
-A = np.dot(R.T, np.dot(Ab, R))
-Bb = np.dot(Q.T, np.dot(BL, Q))
-B = np.dot(R.T, np.dot(Bb, R))
-x = np.dot(R, xb)
-y = np.dot(R, yb)
+Ab = Q.T.dot(AL.dot(Q))
+A = R.dot(Ab.dot(R.T))
+Bb = Q.T.dot(BL.dot(Q))
+B = R.dot(Bb.dot(R.T))
+x = R.dot(xb)
+y = R.dot(yb)
 
 print(x)
-
 J0 = get_J0(x, y)
