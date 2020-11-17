c4fb7c6b600f2be4c0cd4ec69a26fd5b18f18eb2
Christensen <njchris2@illinois.edu>
2020-11-17 17:51:27 -0600

    create no install version

---
+++ sempy/agg_amg/no_install/rcb_cut.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def rcb_cut(x, y):
+    x_min = x.min()
+    x_max = x.max()
+    y_min = y.min()
+    y_max = y.max()
+
+    x_len = x_max-x_min
+    y_len = y_max-y_min
+
+    if x_len > y_len:
+        return rcb_cut_graph(x)
+    else:
+        return rcb_cut_graph(y)
+
+
+def rcb_cut_graph(x):
+    indices = np.argsort(x)
+
+    n = x.shape[0]
+    n_high = int(np.ceil(n/2.0))
+    n_low = n-n_high
+
+    return indices[0:n_low], indices[n_low:n]
