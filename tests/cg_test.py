f6ce59a30ea720d6e2bc04fc2eb47149954ea3b2
Thilina Ratnayaka <thilinarmtb@gmail.com>
2022-09-29 22:32:36 -0500

    Fix flake8 failures in tests/

--- tests/cg_test.py
+++ tests/cg_test.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pytest
 
 from sempy.iterative import cg
 
--- tests/poisson_test.py
+++ tests/poisson_test.py
@@ -1,9 +1,7 @@
 import numpy as np
-import pytest
 
 from sempy.elliptic import elliptic_cg
-from sempy.gradient import (gradient, gradient_2d, gradient_transpose,
-                            gradient_transpose_2d)
+from sempy.gradient import gradient, gradient_transpose
 from sempy.iterative import cg, pcg
 from sempy.mesh import load_mesh
 
@@ -131,17 +129,12 @@ def test_single_element_cg_sin_3d():
 
 
 def test_multi_element_cg_sin_3d():
-    N = 10
-    n = N + 1
-
     mesh = load_mesh("box004.msh")
+    N = 10
     mesh.find_physical_coordinates(N)
     mesh.establish_global_numbering()
     mesh.calc_geometric_factors()
     mesh.setup_mask()
-
-    nelem = mesh.get_num_elems()
-    Np = mesh.Np
 
     X = mesh.get_x()
     Y = mesh.get_y()
@@ -168,46 +161,3 @@ def test_multi_element_cg_sin_3d():
     x_cg, niter = elliptic_cg(mesh, b, tol=1e-8, maxit=1000, verbose=0)
 
     assert np.allclose(x, x_cg, 1e-8)
-
-
-from sempy.meshes.box import box_ab_2d
-
-# def test_poisson_sin_2d():
-#    N=15
-#    n=N+1
-#
-#    X,Y=box_ab_2d(0.,1.,N)
-#    G,J,B=geometric_factors_2d(X,Y,n)
-#
-#    def mask(W):
-#        W=W.reshape((n,n))
-#        W[0,:]=0
-#        W[n-1,:]=0
-#        W[:,0]=0
-#        W[:,n-1]=0
-#        W=W.reshape((n*n,))
-#        return W
-#
-#    def Ax(x):
-#        Ux,Uy=gradient_2d(x,n)
-#
-#        Wx=G[0,0,:]*Ux+G[0,1,:]*Uy
-#        Wy=G[1,0,:]*Ux+G[1,1,:]*Uy
-#
-#        W=gradient_transpose_2d(Wx,Wy,n)
-#        W=mask(W)
-#        return W
-#
-#    x_analytic=np.sin(np.pi*X)*np.sin(np.pi*Y)
-#    x_analytic=mask(x_analytic.reshape((n*n,)))
-#
-#    b_analytic=2*np.pi*np.pi*np.sin(np.pi*X)*np.sin(np.pi*Y)
-#    b_analytic=mask(b_analytic.reshape(n*n,)*B*J)
-#
-#    Minv_=1.0/(B*J)
-#    def Minv(r):
-#        return Minv_*r
-#
-#    x,niter=pcg(Ax,Minv,b_analytic,tol=1e-8,maxit=1000,verbose=0)
-#
-#    assert np.allclose(x,x_analytic,1e-8)
