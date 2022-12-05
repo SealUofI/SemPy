612b69ec40df2cadcc30ae4dd038c2282c2b09b7
Nicholas Christensen <njchris2@illinois.edu>
2022-12-04 18:41:42 -0600

    Run autopep8 on kron.py

--- sempy/kron.py
+++ sempy/kron.py
@@ -9,7 +9,7 @@ def kron_2d(Sy, Sx, U, order=None):
     if all([isinstance(X, np.ndarray) for X in [Sy, Sx]]):
         U = np.einsum('ai,ij,bj->ab', Sy, U, Sx, order=order, optimize=True)
     else:
-        U = Sy@U@Sx.T
+        U = Sy @ U @ Sx.T
 
     return U.reshape((nx * ny,), order=order)
 
@@ -18,24 +18,25 @@ def kron(Sz, Sy, Sx, U, order=None):
     nx, mx = Sx.shape
     ny, my = Sy.shape
     nz, mz = Sz.shape
-    
+
     if all([isinstance(X, np.ndarray) for X in [Sz, Sy, Sx]]):
         U = U.reshape((mz, my, mx), order=order)
-        U = np.einsum('ai,bj,ijk,ck->abc', Sz, Sy, U, Sx, order=order, optimize=True)
+        U = np.einsum('ai,bj,ijk,ck->abc', Sz, Sy, U,
+                      Sx, order=order, optimize=True)
     else:
         U = U.reshape((my * mz, mx), order=order)
-        U = U@Sx.T
+        U = U @ Sx.T
 
         U = U.reshape((mz, my, nx), order=order)
-        
+
         if isinstance(Sy, np.ndarray):
             V = np.einsum('mj,ijk->imk', Sy, U, order=order, optimize=True)
         else:
             V = np.empty((mz, ny, nx), order=order)
             for i in range(mz):
-                V[i, :, :] = Sy@U[i, :, :]
+                V[i, :, :] = Sy @ U[i, :, :]
 
         V = V.reshape((mz, nx * ny), order=order)
-        U = Sz@V
+        U = Sz @ V
 
     return U.reshape((nx * ny * nz,), order=order)
